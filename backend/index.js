79731e6b6688d977002b94515139b5eb6bf5b78b
romeolulu14 <romeolulu14@gmail.com>
2023-07-02 10:53:10 -0400

    web socket

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,9 @@
-console.log("Hello world");
+const WebSocket = require('ws');
+const server = new WebSocket.Server({port: '8080'});
+
+server.on('connection', socket => {
+
+    server.on("message", message => {
+        socket.send("Roger that !" + message);
+    })
+});
